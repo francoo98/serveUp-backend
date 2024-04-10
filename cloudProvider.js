85a48b0fd17084408b13b473fe9a81d2bb52ccaf
Franco Ayala <franco.a98@outlook.com>
2024-04-10 17:24:07 -0300

    agregue obtener servidores de um cloud

--- cloudProvider.js
+++ cloudProvider.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+require('dotenv').config()
 
 class CloudProvider {
     constructor() {
@@ -17,14 +18,44 @@ class UMCloudProvider extends CloudProvider {
         super()
     }
     
-    getServers() {
-        return axios.get('http://nova.openstack.svc.metal.kube.um.edu.ar/v2.1/82e080d7e97f4cfb858eb766b25a9bd6/servers')
-        .then(response => {
-            return response.data
-        })
-        .catch(error => {
+    async authenticate() {
+        const body = {
+            "auth": {
+                "identity": {
+                    "methods": [
+                        "password"
+                    ],
+                    "password": {
+                        "user": {
+                            "name": process.env.UM_CLOUD_USERNAME,
+                            "domain": {
+                                "name": "Default"
+                            },
+                            "password": process.env.UM_CLOUD_PASSWORD,
+                        }
+                    }
+                }
+            }
+        }
+
+        const res = await axios.post('http://keystone.openstack.svc.metal.kube.um.edu.ar/v3/auth/tokens', body)
+        this.tokenID = res.headers['x-subject-token']
+    }
+
+    async getServers() {
+        try {
+            await this.authenticate()
+            const res = await axios.get('http://nova.openstack.svc.metal.kube.um.edu.ar/v2.1/82e080d7e97f4cfb858eb766b25a9bd6/servers',
+            {
+                headers: {
+                    'X-Auth-Token': this.tokenID
+                }
+            })
+            return res.data
+        }
+        catch(error) {
             return error.toJSON()
-        })
+        }
     }
 }
 
