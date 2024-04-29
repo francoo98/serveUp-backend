878c2b6e0c2f262327ff35b7ed4a0bb513f904db
Franco Ayala <franco.a98@outlook.com>
2024-04-29 16:31:19 -0300

    Fixed error in service's app selector

--- api.js
+++ api.js
@@ -11,26 +11,26 @@ const k8sAppsApi = kc.makeApiClient(k8s.AppsV1Api);
 
 app.get('/api/server/', (req, res) => {
     k8sApi.listNamespacedService('default')
-    .then((resK8s) => {
-        let services = []
-        resK8s.body.items.forEach((item, i) => {
-            let service = {}
-            service.name = item.metadata.name
-            if(item.spec.type === "ClusterIP") {
-                service.ip = item.spec.clusterIP
-            }
-            else if(item.spec.type === "LoadBalancer") {
-                service.ip = item.status.loadBalancer.ingress[0].ip
-            }
-            service.port = item.spec.ports[0].port;
-            services.push(service)
+        .then((resK8s) => {
+            let services = []
+            resK8s.body.items.forEach((item, i) => {
+                let service = {}
+                service.name = item.metadata.name
+                if(item.spec.type === "ClusterIP") {
+                    service.ip = item.spec.clusterIP
+                }
+                else if(item.spec.type === "LoadBalancer") {
+                    service.ip = item.status.loadBalancer.ingress[0].ip
+                }
+                service.port = item.spec.ports[0].port;
+                services.push(service)
+            })
+            res.send(services)
         })
-        res.send(services)
-    })
-    .catch((err) => {
-        console.error('Error:', err)
-        res.send(err)
-    });
+        .catch((err) => {
+            console.error('Error:', err)
+            res.send(err)
+        });
 })
 
 app.post('/api/server/', async (req, res) => {
@@ -67,35 +67,33 @@ app.post('/api/server/', async (req, res) => {
         },
     }
 
-    try {
-        const createDeployment = await k8sAppsApi.createNamespacedDeployment('default', deployment)
-      
-        const service = {
-          kind: "Service",
-          apiVersion: "v1",
-          metadata: {
+    const service = {
+        kind: "Service",
+        apiVersion: "v1",
+        metadata: {
             name: "minecraft-service",
-          },
-          spec: {
+        },
+        spec: {
             selector: {
-              app: "minecraft",
+                app: "game-server",
             },
             ports: [{
-              protocol: "TCP",
-              port: 25565,
-              targetPort: 25565,
+                protocol: "TCP",
+                port: 25565,
+                targetPort: 25565,
             }],
             type: "LoadBalancer"
-          }
         }
-      
+    }
+
+    try {
         const createService = await k8sApi.createNamespacedService('default', service)
-        await sleep(10000);
-        console.log(createService.body.status.loadBalancer)
+        const createDeployment = await k8sAppsApi.createNamespacedDeployment('default', deployment)
+        //console.log(createService.body.status.loadBalancer)
         // Por defecto, algunos kube-proxy y servicios solo llenarán 'status.loadBalancer.ingress[0].ip', otros llenarán 'status.loadBalancer.ingress[0].hostname'.
-        const ip = createService.status.loadBalancer.ingress[0].ip || createService.status.loadBalancer.ingress[0].hostname
+        const ip = createService.body.status.loadBalancer.ingress[0].ip || createService.status.loadBalancer.ingress[0].hostname
         res.json({ ip })
-      
+
     } catch (error) {
         console.error(error)
         res.status(500).json({ error: error.toString() })
@@ -103,9 +101,9 @@ app.post('/api/server/', async (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
 
 function sleep(milliseconds) {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
- }
+}
