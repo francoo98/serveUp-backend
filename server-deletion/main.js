973f325f9e5543da0af63948e53182a5a9f230ea
Franco Ayala <franco.a98@outlook.com>
2024-06-10 20:47:45 -0300

    Added an app to delete servers periodically

---
+++ server-deletion/main.js
@@ -0,0 +1,29 @@
+const k8s = require('@kubernetes/client-node')
+
+const kc = new k8s.KubeConfig()
+kc.loadFromDefault()
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api)
+const k8sAppsApi = kc.makeApiClient(k8s.AppsV1Api)
+
+const users = ['franco', 'tomi']
+
+async function deleteServers() {
+    for (const user of users) {
+        const services = await k8sApi.listNamespacedService('user-' + user)
+        const deployments = await k8sAppsApi.listNamespacedDeployment('user-' + user)
+
+        for (const service of services.body.items) {
+            if (new Date() - service.metadata.creationTimestamp > 30000) {
+                k8sApi.deleteNamespacedService(service.metadata.name, 'user-' + user)
+            }
+        }
+
+        for (const deployment of deployments.body.items) {
+            if (new Date() - deployment.metadata.creationTimestamp > 30000) {
+                k8sAppsApi.deleteNamespacedDeployment(deployment.metadata.name, 'user-' + user)
+            }
+        }
+    }
+}
+
+deleteServers()
